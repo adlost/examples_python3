048b2eb9508a5f0c5913aeb3f2581d678c9a06af
andlozedd <andlozedd@gmail.com>
2021-04-15 17:23:38 +0300

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -5,12 +5,10 @@ stuff = {'role': 1, 'torch': 6, "gold coin": 42, "dagger": 1, 'arrow': 12}
 def displayInventory(inventory):
 	print("Inventory:")
 	item_total = 0
-	for k, v in inventory.items():
-		print(str(v) + ' ' + k)
-		item_total += v
+	for key, value in inventory.items():
+		print(str(value) + ' ' + key)
+		item_total += value
 	print("Total number of items: " + str (item_total))
 
 
 displayInventory(stuff)
-
-print(stuff.items())
