41e62aaaf39755d0adc78e83013b2b480b3b2ad2
andlozedd <andlozedd@gmail.com>
2021-04-16 11:25:54 +0300

    Пример обновления словаря

--- dragonloot.py
+++ dragonloot.py
@@ -1,24 +1,29 @@
 inv = {'gold coin': 42, 'rope': 1}
-
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin']
-
-
-def displayInventory(inventory):
-	print("Inventory:")
-	item_total = 0
-	for key, value in inventory.items():
-		print(str(value) + ' ' + key)
-		item_total += value
-	print("Total number of items: " + str (item_total))
 
 
 def addToInventory(inventory, addeditem):
 	dirloot={}
+	#тут мы делаем из списка словарь
 	for i in addeditem:
 		dirloot.update({i: addeditem.count(i)})
-	
+
+	#тут творится магия)
+	# цикл переперает ключи из лута
+	for k, v in dirloot.items():
+		var_loot = dirloot.get(k)
+		var_inv = inventory.get(k)
+		#проверяем есть ли в словаре ключ который есть в луте
+		#если ключа нет, переменной присвается значение 0   
+		if var_inv== None:
+			var_inv = 0
+		#тут обновляется значение для ключа
+		var_fin = var_loot+var_inv
+		inventory.update({k: var_fin})
+	print(inventory)
 
 
-
+print(inv)
+print(dragonLoot)
 
 addToInventory(inv, dragonLoot)
