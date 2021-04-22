c0957848940b8e9cf6a91a517e40ad57ed56cee7
Administrator <admin@example.com>
2021-04-22 18:09:47 +0300

    test

---
+++ folderCheck.py
@@ -0,0 +1,12 @@
+import os
+
+for folderName, subfolders, filenames in os.walk('/home/and/python/examples_python3'):
+	print ('Текущая директория - ' + folderName)
+
+	for subfolder in subfolders:
+		print('Подпапка директории ' + folderName + ': ' + subfolder)
+
+	for filename in filenames:
+		 print('Файл в папке ' + subfolder + ': ' + filename)
+
+	print('')
